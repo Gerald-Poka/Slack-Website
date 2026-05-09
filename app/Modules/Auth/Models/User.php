cb434c2ab9e8422422993ce31b0377698ca4af61
geraldndyamukama <gerald.ndyamukama@ucc.co.tz>
2026-05-09 03:06:44 +0300

    Fix: Fixing critical issue based on CI

--- app/Modules/Auth/Models/User.php
+++ app/Modules/Auth/Models/User.php
@@ -20,9 +20,6 @@ class User extends Authenticatable
         'password',
         'avatar',
         'status',
-        'two_factor_secret',
-        'two_factor_recovery_codes',
-        'two_factor_confirmed_at',
         'last_login_at',
         'last_login_ip',
     ];
